49736d8202b0888db6f449e94ea45bd6f2b2c8ed
vneting <tyuiop7506@gmail.com>
2025-02-21 11:32:25 +0900

    변수 연습 3

---
+++ Likelion_04/Likelion_04/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Likelion_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*            string gret;
+                        gret = "hello world";
+
+                        console.writeline(gret);*/
+
+            /*int score = 100;
+            float tem = 36.5f;
+            string city = "seoul";
+
+            Console.WriteLine(score);
+            Console.WriteLine(tem);
+            Console.WriteLine(city);*/
+
+            /*int x = 10, y = 20, z = 30;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);*/
+
+            const double pi = 3.141592;
+            const int Mscore = 100;
+
+            Console.WriteLine("Pi="+pi);
+            Console.WriteLine("Max Score="+Mscore);
+        }
+    }
+}
