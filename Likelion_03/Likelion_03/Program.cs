2f05a248032d0cd4ffdd3dda45a3492c1eba3d35
vneting <tyuiop7506@gmail.com>
2025-02-21 10:41:34 +0900

    변수 연습

---
+++ Likelion_03/Likelion_03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Likelion_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hp = 100;
+            double att = 56.7;
+            string name = "Lion";
+            char grade = 'S';
+
+            Console.WriteLine(hp);
+            Console.WriteLine(att);
+            Console.WriteLine(name);
+            Console.WriteLine(grade);
+        }
+    }
+}
